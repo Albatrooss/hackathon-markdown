cd59a7dda8375494d8798495d95feabe6fee616c
Albatrooss <robillardtim@gmail.com>
2020-05-26 15:11:11 -0400

    added menu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+let menuShowing = false;
+
 const myOutput = document.getElementById('output');
 const myInput = document.getElementById('input');
 const menu = document.getElementById('menu');
@@ -5,15 +7,12 @@ const menuBtn = document.getElementById('menu-btn');
 
 const bgColor = document.getElementById('input-background-color');
 const fontColor = document.getElementById('input-font-color');
-const h1Size = document.getElementById('input-h1-size');
-const h2Size = document.getElementById('input-h2-size');
-const pSize = document.getElementById('input-p-size');
 
 const markConvert = new showdown.Converter();
 
 input.addEventListener('input', convert);
 menuBtn.addEventListener('click', toggleMenu);
-menu.addEventListener('keydown', updateStyle);
+menu.addEventListener('input', updateStyle);
 // bgColor.addEventListener('keydown', updateStyle);
 
 function convert() {
@@ -24,21 +23,38 @@ function convert() {
 
 function toggleMenu() {
   menu.classList.toggle('visible');
+  if (menuShowing) {
+    menuBtn.classList.toggle('menu-clicked');
+    menuShowing = false;
+  } else {
+    setTimeout(() => menuBtn.classList.toggle('menu-clicked'), 250);
+    menuShowing = true;
+  }
 }
 
 function updateStyle(e) {
   let input = e.target.value;
-  if (e.key !== 'Enter') return;
   if (e.target.id === 'input-background-color') {
     if (input.length === 3 || input.length === 6 || input.length === 0) {
       myOutput.style.backgroundColor = input.length === 0 ? '#ffffff' : `#${input}`;
+      document.getElementById('input-background-color').style.backgroundColor =
+        input.length === 0 ? '#ffffff' : `#${input}`;
     }
   } else if (e.target.id === 'input-font-color') {
     if (input.length === 3 || input.length === 6 || input.length === 0) {
       myOutput.style.color = input.length === 0 ? '#ffffff' : `#${input}`;
+      document.getElementById('input-font-color').style.color =
+        input.length === 0 ? '#ffffff' : `#${input}`;
     }
   } else if (e.target.id === 'input-h1-size') {
-    myOutput.style.backgroundColor = input.length === 0 ? '#ffffff' : `#${input}`;
+    let h1Els = document.querySelectorAll('#output h1');
+    h1Els.forEach(x => (x.style.fontSize = input));
+  } else if (e.target.id === 'input-h2-size') {
+    let h2Els = document.querySelectorAll('#output h2');
+    h2Els.forEach(x => (x.style.fontSize = input));
+  } else if (e.target.id === 'input-p-size') {
+    let pEls = document.querySelectorAll('#output p');
+    pEls.forEach(x => (x.style.fontSize = input));
   }
 }
 
